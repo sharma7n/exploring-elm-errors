00a4a7c74424927ab4663ce3033a4fd3595486d8
Nik Sharma <sharma7n@gmail.com>
2019-03-13 03:26:14 +0000

    content

--- src/Main.elm
+++ src/Main.elm
@@ -16,6 +16,13 @@ main = Slides.app
         
         Elm in the Spring 2019
         """
+    , Slides.md
+        """
+        - My story of discovering a cool error message
+        - An overview of all the kinds of errors in Elm
+        - One code example per error type
+        - Takeaways
+        """
     , Slides.html <|
         (Html.div
             []
@@ -24,4 +31,56 @@ main = Slides.app
                 ] 
                 []
             ])
+    , Slides.html <|
+        (Html.div
+            []
+            [ Html.img
+                [ Attributes.src "assets/advice-bot.png"
+                ]
+                []
+            ])
+    , Slides.md
+        """
+        bot.animechicago.com
+        """
+    , Slides.md
+        """
+        Source: https://github.com/elm/compiler/blob/master/compiler/src/Reporting/Error.hs
+        ```
+        -- ALL POSSIBLE ERRORS
+
+
+        data Error
+          = Syntax Syntax.Error
+          | Canonicalize Canonicalize.Error
+          | Type L.Localizer [Type.Error]
+          | Main L.Localizer Main.Error
+          | Pattern [Pattern.Error]
+          | Docs Docs.Error
+        ```
+        """
+    , Slides.md
+        """
+        # Syntax Errors
+        """
+    , Slides.md
+        """
+        # Canonicalize Errors
+        """
+    , Slides.md
+        """
+        # Type Errors
+        """
+    , Slides.md
+        """
+        # Main Errors
+        """
+    , Slides.md
+        """
+        # Pattern Errors
+        """
+    , Slides.md
+        """
+        # Docs Errors
+        """
     ]
